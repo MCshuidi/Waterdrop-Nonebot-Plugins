1b7ff63173e48a3c30e142da47d61adcd53a8df8
MCshuidi <24938120@qq.com>
2025-03-15 13:02:55 +0800

    Version 1.0.0:0011 : Fixed some bugs

--- qqbot/plugins/randoms/TRPG/ra.py
+++ qqbot/plugins/randoms/TRPG/ra.py
@@ -77,4 +77,4 @@ async def ra_handle(bot : Bot , event : MessageEvent | GroupMessageEvent):
         r_type = rnd.choice(result_texts[3])
     else:
         r_type = rnd.choice(result_texts[4])
-    await ra_dice.finish(f"正在为[{(await bot.get_stranger_info(user_id = int(event.get_user_id())))["nick"]}]进行技能[{type.strip()}:{result[3]}]的检定。\n1D100={dice}/{point} \n{r_type}")
+    await ra_dice.finish(f"正在为[{(await bot.get_stranger_info(user_id = int(event.get_user_id())))["nick"]}]进行技能[{type.strip()}:{pt}]的检定。\n1D100={dice}/{point} \n{r_type}")
